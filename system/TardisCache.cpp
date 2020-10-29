652065bf6a5a852288bb6fdc33619d90de96b9df
jinzhang <437629012@qq.com>
2020-10-29 13:52:18 +0800

    two files related to Tardis

---
+++ system/TardisCache.cpp
@@ -0,0 +1,5 @@
+#include "TardisCache.hpp"
+
+
+DEFINE_int32(cache_proto, GRAPPA_VANILLA, "CC protocol");
+DEFINE_int32(lease, 10, "The lease of Tardis protocol.");
---
+++ system/TardisCache.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include <bitset>
+#include <list>
+#include <unordered_map>
+#include <gflags/gflags.h>
+#include <string.h>
+
+//#define TARDIS_TWO_STAGE_RENEWAL
+//#ifdef TARDIS_BG_RENEWAL
+// Cache protocol. Only one of them can be defined
+enum cache_proto_t { GRAPPA_VANILLA = 0, GRAPPA_TARDIS, GRAPPA_WI };
+static const char* cache_proto_str[] =  { "Vanilla", "Tardis", "Write-Invalidation" };
+// The definiations are given in TardisCache.cpp
+DECLARE_int32(cache_proto);
+static const int MAX_CACHE_NUMBER = 160;
+DECLARE_int32(lease);
+
+typedef uint32_t timestamp_t;
+
+namespace Grappa {
+namespace impl {
+
+struct cache_info_base {
+  cache_info_base() : refcnt(0), usedcnt(0), size(0), object(nullptr) {}
+  cache_info_base(void* obj, size_t sz) : object(obj), size(sz),
+    refcnt(0), usedcnt(0) {}
+  // How many actived tasks (might access internal data of cache_info) are there?
+  mutable char refcnt;
+  // How many tasks who holds the reference to cache_info are there?
+  mutable char usedcnt;
+  // These two fields implements template parameters.
+  mutable size_t size;
+  mutable void* object;
+  // O(1) remove/insertion time for LRU list.
+  mutable std::list<uintptr_t>::iterator lru_iter;
+
+  void assign(const void* obj) {
+    // This object has been freed.
+    if (object == nullptr) return;
+    memcpy(object, obj, size);
+  }
+  void* get_object() { return object; }
+};
+
+template <typename T>
+struct lock_obj { T object; bool locked; };
+
+struct tardis_owner_cache_info {
+  tardis_owner_cache_info() : rts(0), wts(0) {}
+  timestamp_t rts, wts;
+};
+
+struct tardis_cache_info : cache_info_base {
+  tardis_cache_info() : cache_info_base() {}
+  tardis_cache_info(void *obj, size_t sz) : cache_info_base(obj, sz),
+    rts(0), wts(0) {}
+  mutable timestamp_t rts, wts;
+};
+
+template< typename T >
+struct rpc_read_result {
+  rpc_read_result(T _r, const tardis_owner_cache_info& c) : r(_r), rts(c.rts),
+    wts(c.wts) {}
+  rpc_read_result() {}
+  timestamp_t rts, wts;
+  T r;
+};
+
+struct wi_owner_cache_info {
+  std::bitset<MAX_CACHE_NUMBER> copyset;
+  // Whether this object is locked globally.
+  bool locked;
+  wi_owner_cache_info(): locked(false) {}
+};
+
+struct wi_cache_info : cache_info_base {
+  bool valid;
+  wi_cache_info() : cache_info_base(), valid(false) {}
+  wi_cache_info(void *obj, size_t sz) : cache_info_base(obj, sz),
+    valid(false) {}
+};
+
+}
+}
+using tardis_o_t = Grappa::impl::tardis_owner_cache_info;
+using wi_o_t = Grappa::impl::wi_owner_cache_info;
+using tardis_c_t = Grappa::impl::tardis_cache_info;
+using wi_c_t = Grappa::impl::wi_cache_info;
